e5e49d737706819c02a240527b714ad532aaef6b
vkwan-teau <vkwan-teau@hotmail.fr>
2016-05-25 04:19:18 -0400

    exercice 1 + exercice 2

--- personne.php
+++ personne.php
@@ -29,7 +29,38 @@ class personne {
   }
 }
 
-$vincent = new personne(1,"Kwan-Teau","Vincent");
-echo $vincent->__toString();
+class contact {
+	protected $id;
+	protected $nom;
+	protected $prenom;
+  protected $email;
+  
+    function __construct ($id,$nom,$prenom,$email){
+      $this->id = $id;
+      $this->nom = $nom;
+      $this->prenom = $prenom;
+      $this->email = $email;
+    }
+  
+  public function getPrenom (){
+    return $this->prenom; 
+  }
+  public function getNom (){
+    return $this->nom;
+  }
+  public function setPrenom ($prenom){
+    $this->prenom = $prenom;
+  }
+  public function setNom ($nom){
+    $this->nom = $nom;
+  }
+  
+  function __toString (){
+    return "prenom = ".$this->prenom." / nom = ".$this->nom." / id = ".$this->id." / email= ".$this->email;
+  }
+}
+
+$vincent = new contact(1,"Kwan-Teau","Vincent","vkwan-teau@hotmail.fr");
+echo $vincent
 
 ?>
